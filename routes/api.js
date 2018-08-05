c8e04ea46548f1d2e333a545d71aff4b9ed8119b
Daniel Lewis <dlewismcr@gmail.com>
2018-08-05 17:33:02 +0100

    added procfile

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,7 @@ const topicsRouter = require("./topics.js");
 const articlesRouter = require("./articles.js");
 const commentsRouter = require("./comments.js");
 const usersRouter = require("./users.js");
-const homepage = require("../public/homepage.html");
 
-apiRouter.route("/").get(homepage);
 apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/articles", articlesRouter);
 apiRouter.use("/comments", commentsRouter);
